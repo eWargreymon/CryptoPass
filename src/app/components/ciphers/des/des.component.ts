dbf81a9f381c40c82cc8be562504eca4971a10e2
eWargreymon <eddyhb1@hotmail.com>
2021-05-20 21:26:24 +0100

    retoques en metodos de cifrado y pagina principal

--- src/app/components/ciphers/des/des.component.ts
+++ src/app/components/ciphers/des/des.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ClipboardService } from 'ngx-clipboard';
 import { ComunicationService } from 'src/app/services/comunication.service';
 
 @Component({
@@ -16,7 +17,14 @@ export class DesComponent implements OnInit {
   cipherPass: string;
   descifrado: string;
 
-  constructor(private comunication: ComunicationService) { }
+  generado: boolean;
+  mostrar: boolean;
+
+  mode: boolean;
+
+  constructor(private comunication: ComunicationService, private clipboardApi: ClipboardService) { 
+    this.mode = true; 
+  }
 
   ngOnInit(): void {
     this.comunication.dataCifrado.subscribe(mensaje => {
@@ -29,11 +37,28 @@ export class DesComponent implements OnInit {
   }
 
   cipher(){
-    this.cifrado = this.comunication.toDES(this.mensaje, this.clave);
+    
+    if(this.mensaje && this.clave){
+      this.cifrado = this.comunication.toDES(this.mensaje, this.clave);
+      this.generado = true;
+    } else {
+      alert('No se ha introducido texto');
+    }
   }
 
   decrypt(){
     this.descifrado = this.comunication.fromDES(this.cipherText, this.cipherPass);
+  }
+  
+  copyText() {
+    this.clipboardApi.copyFromContent(this.cifrado);
+  }
+
+  reset(){
+    this.generado = false;
+    this.mostrar = false;
+    this.mensaje = '';
+    this.clave = '';
   }
 
 }
