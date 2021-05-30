e560b2f8866fa7a0b954472c8b623e11bc992671
eWargreymon <eddyhb1@hotmail.com>
2021-05-30 02:58:23 +0100

    deployment and firebase settings for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+  apiKey: "AIzaSyAe1FPx2R0jrk_9yxkKF7TrNgYbMT7wDsA",
+  authDomain: "login-crypto-cfedd.firebaseapp.com",
+  projectId: "login-crypto-cfedd",
+  storageBucket: "login-crypto-cfedd.appspot.com",
+  messagingSenderId: "414006404627",
+  appId: "1:414006404627:web:6cd432aeb51dc5a69481e6"
+  }
 };
